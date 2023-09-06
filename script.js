5f07b408623262b159439e2cdcf3e743cfe3748e
135791maria <dossantosmariadapenha05@gmail.com>
2023-09-06 20:12:58 -0300

    novo convert money

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let currencyValue = document.querySelector("currency-value")
+
+
+function calcularDolar(){
+
+    let real = document.getElementById("input-real").value 
+    let dolar = document.getElementById("input-dolar").value
+    
+    let resultado = real / dolar
+    alert(resultado)
+
+ 
+dolares.innerHTML = "$" + 20
+alert("Ó resultado é $" + 20 + "dólares")
+
+   
+}
+
+
+
+
+
+
+        
+    
